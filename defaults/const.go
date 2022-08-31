b67cda5f0f42ac7130aa5a84d7d153f73e9e0011
MickMake <github@mickmake.com>
2022-08-31 19:55:19 +1000

    Committed v1.0.1

--- defaults/const.go
+++ defaults/const.go
@@ -3,7 +3,7 @@ package defaults
 const (
 	Description   = "GoWhen - CLI based Date/Time manipulation written in GoLang"
 	BinaryName    = "GoWhen"
-	BinaryVersion = "1.0.0"
+	BinaryVersion = "1.0.1"
 	SourceRepo    = "github.com/MickMake/" + BinaryName
 	BinaryRepo    = "github.com/MickMake/" + BinaryName
 
