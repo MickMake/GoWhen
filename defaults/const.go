ee9ed63493d71b91166ce93a5b6f6edee17165b1
MickMake <github@mickmake.com>
2026-05-26 22:25:30 +1000

    Update const.go

--- defaults/const.go
+++ defaults/const.go
@@ -5,6 +5,7 @@ import _ "embed"
 // Need to execute `go generate -v -x defaults/const.go` OR `go generate -v -x ./...`
 //go:generate cp ../README.md README.md
 //go:generate cp ../EXAMPLES.md EXAMPLES.md
+//go:generate cp ../FILTERS.md FILTERS.md
 
 //go:embed README.md
 var Readme string
@@ -15,7 +16,7 @@ var Examples string
 const (
 	Description   = "GoWhen - CLI based Date/Time manipulation written in GoLang"
 	BinaryName    = "GoWhen"
-	BinaryVersion = "1.0.5"
+	BinaryVersion = "1.1.0"
 	SourceRepo    = "github.com/MickMake/" + BinaryName
 	BinaryRepo    = "github.com/MickMake/" + BinaryName
 
