0ab96eac53d0b89e395de0f02300f189e2416a26
MickMake <github@mickmake.com>
2026-05-28 03:38:37 +1000

    Add named format output aliases

--- cmd/filter_print.go
+++ cmd/filter_print.go
@@ -38,39 +38,47 @@ func (cs *Cmds) FilteredPrintTime(t time.Time) {
 			break
 		}
 
-		if cs.Data.Format == "epoch" {
-			fmt.Printf("%d\n", t.Unix())
-			break
-		}
+		cs.PrintFormattedTime(t)
+	}
+}
 
-		if cs.Data.Format == "week" {
+func (cs *Cmds) PrintFormattedTime(t time.Time) {
+	for range Only.Once {
+		switch cs.Data.Format {
+		case "epoch", "unix":
+			fmt.Printf("%d\n", t.Unix())
+		case "unix-ms":
+			fmt.Printf("%d\n", t.UnixNano()/int64(time.Millisecond))
+		case "unix-us":
+			fmt.Printf("%d\n", t.UnixNano()/int64(time.Microsecond))
+		case "unix-ns":
+			fmt.Printf("%d\n", t.UnixNano())
+		case "week":
 			_, w := t.ISOWeek()
 			fmt.Printf("%d\n", w)
-			break
-		}
-
-		if cs.Data.Format == "list" || cs.Data.Format == "cal-week" {
+		case "list", "cal-week":
 			m := cal.New(t).Week()
 			m.Print()
-			break
-		}
-
-		if cs.Data.Format == "cal-month" {
+		case "cal-month":
 			m := cal.New(t).Month()
 			m.Print()
-			break
-		}
-
-		if cs.Data.Format == "cal-year" {
+		case "cal-year":
 			y := cal.New(t).Year()
 			y.Print()
-			break
+		case "iso":
+			fmt.Printf("%s\n", t.Format(time.RFC3339))
+		case "date":
+			fmt.Printf("%s\n", t.Format("2006-01-02"))
+		case "time":
+			fmt.Printf("%s\n", t.Format("15:04:05"))
+		case "datetime":
+			fmt.Printf("%s\n", t.Format("2006-01-02 15:04:05"))
+		default:
+			if cs.Data.Format == "" {
+				cs.Data.Format = time.RFC3339Nano
+			}
+			fmt.Printf("%s\n", t.Format(cs.Data.Format))
 		}
-
-		if cs.Data.Format == "" {
-			cs.Data.Format = time.RFC3339Nano
-		}
-		fmt.Printf("%s\n", t.Format(cs.Data.Format))
 	}
 }
 
@@ -88,7 +96,7 @@ func (cs *Cmds) FilteredPrintRange() {
 		if cs.Data.ToDate.Time.Before(*cs.Data.FromDate.Time) {
 			for t := *cs.Data.FromDate.Time; t.After(*cs.Data.ToDate.Time); {
 				if cs.Data.ShouldPrint(t) {
-					fmt.Println(t.Format(cs.Data.Format))
+					cs.PrintFormattedTime(t)
 				}
 				t = t.AddDate(-int(cs.Data.Range.Years), -int(cs.Data.Range.Months), 0).Add(-cs.Data.Range.Time)
 				if lt == t {
@@ -101,7 +109,7 @@ func (cs *Cmds) FilteredPrintRange() {
 
 		for t := *cs.Data.FromDate.Time; t.Before(*cs.Data.ToDate.Time); {
 			if cs.Data.ShouldPrint(t) {
-				fmt.Println(t.Format(cs.Data.Format))
+				cs.PrintFormattedTime(t)
 			}
 			t = t.AddDate(int(cs.Data.Range.Years), int(cs.Data.Range.Months), 0).Add(cs.Data.Range.Time)
 			if lt == t {
