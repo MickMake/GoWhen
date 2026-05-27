b009c6b8912fb98e953da0764fdb32d0f04b3c71
MickMake <github@mickmake.com>
2026-05-28 06:30:12 +1000

    Remove filters help command attachment

--- cmd/commands.go
+++ cmd/commands.go
@@ -89,7 +89,6 @@ func init() {
 		cmds.Alias.AttachCommand(cmdRoot)
 
 		AttachFilterCommands(cmdRoot)
-		AttachFilterHelpCommand(cmdRoot)
 
 		cmds.Data.GoFormat = true
 		cmds.Data.CppFormat = false
